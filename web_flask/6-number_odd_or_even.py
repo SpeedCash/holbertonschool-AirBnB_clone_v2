6ac6f567c48ac2f37399a664e07b7f9cf5ae43c9
SpeedCash <tcravero@gmail.com>
2024-04-09 15:06:07 +0000

    6. Odd or even?

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,45 +1,53 @@
 #!/usr/bin/python3
+"""starts a Flask web application"""
 from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """display Hello HBNB!"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """display hbnb"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    return 'C ' + text.replace('_', ' ')
+def c(text):
+    """display /c/<text>"""
+    return 'C ' + text.replace('_' + ' ')
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_is_cool(text):
+def python(text):
+    """display /python/<text>"""
     return 'Python ' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def is_it_a_number(n):
-    return '{} is a number'.format(n)
+def number(n):
+    """display “n is a number” only if n is an integer"""
+    return f"{n} is a number"
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
+    """display a HTML page only if n is an integer"""
     return render_template('5-number.html', number=n)
 
 
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
-    odd_or_even = 'odd' if n % 2 else 'even'
-    return render_template('6-number_odd_or_even.html',
+    """display a HTML page only if n is an integer"""
+    odd_or_even = 'even' if n % 2 == 0 else 'odd'
+    return render_template("6-number_odd_or_even.html",
                            number=n, odd_or_even=odd_or_even)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
