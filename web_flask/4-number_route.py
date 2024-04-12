81ecc879172c89720bd594b14b2bcdd417a0be80
SpeedCash <tcravero@gmail.com>
2024-04-12 09:55:35 +0000

    updated all files with a last line

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -51,3 +51,4 @@ def n_is_integer(n):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+    
--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -58,3 +58,4 @@ def n_template(n):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+    
